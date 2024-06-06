c9f6b649f0348177187cfada27021f4c6523d2f3
Quan Ming <116425066+wahpiangle@users.noreply.github.com>
2024-06-06 23:36:38 +0800

    Initial Commit

---
+++ swift-tutorial/swift_tutorialApp.swift
@@ -0,0 +1,17 @@
+//
+//  swift_tutorialApp.swift
+//  swift-tutorial
+//
+//  Created by Tan Quan Ming on 06/06/2024.
+//
+
+import SwiftUI
+
+@main
+struct swift_tutorialApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
